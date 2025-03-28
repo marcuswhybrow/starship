bdbcfa9e43a3e7b8f737b5850c3bf9402629ef9b
Marcus Whybrow <marcus@whybrow.uk>
2025-03-28 07:09:28 +0000

    Make frequent modules gray + remove icons

--- flake.nix
+++ flake.nix
@@ -9,7 +9,34 @@
     configFile = tomlFormat.generate "starship.toml" {
       nix_shell = {
         heuristic = true; 
+        pure_msg = "pure";
+        impure_msg = "impure";
+        symbol = "";
+        style = "fg:8"; # brightblack (grey)
+        format = "[nix/$state]($style)";
       };
+      git_branch.disabled = true;
+      git_commit.disabled = true;
+      git_state.disabled = true;
+      git_metrics.disabled = true;
+      git_status.disabled = true;
+      rust = {
+        symbol = " ";
+        style = "fg:8";
+        format = "[rust/$version]($style) ";
+      };
+      nodejs = {
+        symbol = "󰎙 ";
+        style = "fg:8";
+        format = "[node/$version]($style) ";
+        detect_folders = []; # prevents triggering on node_modules
+      };
+      package = {
+        symbol = "󰏗 ";
+        style = "fg:8";
+        format = "[$version]($style) ";
+      };
+      golang.format = "[go/$version](fg:8) ";
     };
 
     wrapper = pkgs.runCommand "starship-wrapper" {
